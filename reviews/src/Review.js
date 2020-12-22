eb6d09f4b044a077bc85184baf81dd835e6b663c
Mario <marketingkg@mail.ru>
2020-12-22 13:54:54 +0600

    reviews are done

--- reviews/src/Review.js
+++ reviews/src/Review.js
@@ -1,17 +1,50 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaDivide, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import {
+  FaChevronLeft,
+  FaChevronRight,
+  FaDivide,
+  FaQuoteRight,
+} from "react-icons/fa";
 
 const Review = () => {
-  const [review , setReview] = useState(0)
-  const {image,name} = people[review]
+  const [review, setReview] = useState(0);
+  const { image, name, text, job } = people[review];
   return (
-    <div className="article">
+    <article className="review">
       <div className="img-container">
-        <img src={image} className="person-img"/>
+        <img src={image} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
       </div>
-    </div>
-  )
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button
+          className="prev-btn"
+          onClick={() => {
+            review - 1 < 0
+              ? setReview(people.length - 1)
+              : setReview((prevReview) => prevReview - 1);
+          }}
+        >
+          <FaChevronLeft />
+        </button>
+        <button
+          className="next-btn"
+          onClick={() => {
+            review + 1 >= people.length
+              ? setReview(0)
+              : setReview((prevReview) => prevReview + 1);
+          }}
+        >
+          <FaChevronRight />
+        </button>
+      </div>
+    </article>
+  );
 };
 
 export default Review;
