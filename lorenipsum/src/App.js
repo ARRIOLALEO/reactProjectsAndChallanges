b06c5b1c15e6939c6a4462110af0aaa8f6b0a20c
Mario <marketingkg@mail.ru>
2020-12-28 14:16:20 +0600

    remations

--- lorenipsum/src/App.js
+++ lorenipsum/src/App.js
@@ -5,28 +5,34 @@ function App() {
   const [text, setText] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
-    let numberOfLorensIpsums = e.target.amount.value
-    if(numberOfLorensIpsums <= 0){
-      numberOfLorensIpsums = 1
-    }else if(numberOfLorensIpsums >= data.length - 1){
-      numberOfLorensIpsums = data.length 
+    let numberOfLorensIpsums = e.target.amount.value;
+    if (numberOfLorensIpsums <= 0) {
+      numberOfLorensIpsums = 1;
+    } else if (numberOfLorensIpsums >= data.length - 1) {
+      numberOfLorensIpsums = data.length;
     }
- setText([...data.slice(0, numberOfLorensIpsums)])
+    setText([...data.slice(0, numberOfLorensIpsums)]);
   };
-let lorenIpsumsToPrint = text.map(lorenIpsum => <p>{lorenIpsum}</p>)
+  let lorenIpsumsToPrint = text.map((lorenIpsum) => <p>{lorenIpsum}</p>);
   return (
     <section className="section-center">
       <h3> tired od Boring Loren Ipsum?</h3>
       <form className="lorem-form" onSubmit={handleSubmit}>
-        <label htmlFor="amount">
-          paragraps
-        </label>
-        <input type="number" name="amount" id="amount" value={paragraps} onChange={(e)=>{
-          setParagraps(e.target.value)
-        }}/>
-        <button type="submit" className="btn" >Create</button>
+        <label htmlFor="amount">paragraps</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={paragraps}
+          onChange={(e) => {
+            setParagraps(e.target.value);
+          }}
+        />
+        <button type="submit" className="btn">
+          Create
+        </button>
       </form>
-      {lorenIpsumsToPrint }
+      {lorenIpsumsToPrint}
     </section>
   );
 }
