79c2483b0791e49eb8b6d5d6edf56b76d53f68f4
Mario <marketingkg@mail.ru>
2020-12-30 07:16:24 +0600

    colorPAlete finish

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -1,10 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect} from 'react'
 import SingleColor from './SingleColor'
 
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] =  useState('')
+  const [error, setError] = useState(false)
+  const [list,   setList] = useState([])
+  const handleSummit = (e)=>{
+    e.preventDefault()
+    try{
+      let colorToLock = new Values(color).all(10)
+      setList(colorToLock)
+      setError(false)
+    }catch(err){
+      setColor("#f15025")
+      setError(true)
+    }
+    
+  }
+  return (
+    <>
+    <container className="container">
+      <h3>Color Generator</h3>
+      <form onSubmit={handleSummit}>
+        <input type="text" value={color} onChange={(e)=>{
+          setColor(e.target.value)
+        }} placeholder="#f15025" className={error === true && "error"}/>
+        <button className="btn" type="submit">Submit</button>
+      </form>
+    </container>
+    <section className="colors">
+      
+     {list.map((color,index)=>{
+       return <SingleColor key={index} {...color} index={index}/>
+     })}
+       </section>
+    </>
+  )
 }
 
 export default App
