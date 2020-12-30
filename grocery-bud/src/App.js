1787c5e7594f09b0998fd6d451807e5da5904526
Mario <marketingkg@mail.ru>
2020-12-30 17:22:05 +0600

    finish grocerrybud

--- grocery-bud/src/App.js
+++ grocery-bud/src/App.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
-
+const getListLocalStorage = () =>{
+  let list = localStorage.getItem('list')
+  if(list){
+    return JSON.parse(localStorage.getItem('list'))
+  }else{
+    return []
+  }
+}
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getListLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
@@ -12,10 +19,9 @@ function App() {
     e.preventDefault();
     if(!name){
       showAller(true,'Add some Information here', 'danger')
-    }else if(name && isEditing){
-      // replace 
     }else{
       showAller(true,'Added to the List', 'success')
+      isEditing && setIsEditing(false)
       const newItem = {id: new Date().getTime().toString(),title:name}
       setList(prevList =>(prevList = [...prevList,newItem]))
       setName('')
@@ -34,10 +40,14 @@ function App() {
     showAller(true,'Editing element', 'success')
     let getElementToModify = list.filter(element => element.id === id)
     console.log(getElementToModify[0].title)
+    setIsEditing(true)
     removeItems(id)
     showAller(true,'Editing element', 'success')
     setName(getElementToModify[0].title)
   }
+  useEffect(() => {
+   localStorage.setItem('list', JSON.stringify(list))
+  }, [list])
   return (
     <section className="section-center">
       <form onSubmit={handleSubmit} className="grocery-form">
