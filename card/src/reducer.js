ddc9a15efad6cb8e2dca924505d45225b618525e
Mario <marketingkg@mail.ru>
2021-01-11 12:30:21 +0600

    uploadservices

--- card/src/reducer.js
+++ card/src/reducer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { act } from 'react-dom/test-utils'
 
 export default function reducer(state,action) {
    if(action.type === 'CLEAR_CART'){
@@ -7,6 +8,25 @@ export default function reducer(state,action) {
    if(action.type === 'REMOVE_ITEM'){  
        let elementsOfCart = [...state.cart].filter(element => element.id !== action.payload)
        return {...state, cart:elementsOfCart}
+   }
+   if(action.type === 'ADD_PRODUCT'){
+    let tempCart = state.cart.map((element)=>{
+        if(element.id === action.payload){
+             return {...element , amount:element.amount++}
+        }
+        return element
+    })
+       return {...state, cart:tempCart}
+   }
+
+   if(action.type === 'REMOVE'){
+       let tempCart = state.cart.map((element)=>{
+           if(element.id === action.payload){
+               return {...element, amount:element.amount--}
+           }
+           return element
+       }).filter(element => element.amount !== 0)
+       return {...state,cart:tempCart}
    }
     return state
 }
